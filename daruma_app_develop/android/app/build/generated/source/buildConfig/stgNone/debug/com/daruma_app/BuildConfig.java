5b3fcba131288cc9d1004da6489a326aed7af682
giangtth-mirabo <giangtth@mirabo.com.vn>
2021-06-22 20:56:22 +0700

    fix empty

---
+++ daruma_app_develop/android/app/build/generated/source/buildConfig/stgNone/debug/com/daruma_app/BuildConfig.java
@@ -0,0 +1,29 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.daruma_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sukoyakadaruma.liferecord.staging";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "stgNone";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  public static final String FLAVOR_api = "stg";
+  public static final String FLAVOR_signing = "none";
+  // Fields from default config.
+  public static final String API_URL = "https://staging.app-api.sukoyakadaruma.com";
+  public static final String ENV = "stg";
+  public static final String FITBIT_CLIENT_ID = "22D4ZD";
+  public static final String FITBIT_CLIENT_SECRET = "675e0fb5ce5a6e8c390d10e0cc5836e6";
+  public static final String FITBIT_REDIRECT_URL = "com.sukoyakadaruma.liferecord.staging://fitbit";
+  public static final String OMRON_AUTH_URL = "https://data-stg-jp.omronconnect.mobi/api/apps/bdf72f34/oauth2/authorize";
+  public static final String OMRON_CLIENT_ID = "a9f0mbrp9kos1qen4j927eh326tnrnnmrnbi";
+  public static final String OMRON_CLIENT_SECRET = "fbu2jekp60vt39l84amltnln16eb5qk90lurj9kgt596qtta3keank62ljoi8bdd";
+  public static final String OMRON_REDIRECT_URL = "com.sukoyakadaruma.liferecord.staging://omron";
+  public static final String OMRON_TOKEN_STATE = "sjp";
+  public static final String OMRON_TOKEN_URL = "https://data-stg-jp.omronconnect.mobi/api/apps/bdf72f34/oauth2/token";
+  public static final String VER_CODE = "1";
+  public static final String VER_NAME = "1.0.0";
+}
---
+++ daruma_app_develop/android/app/src/main/java/com/daruma_app/MainApplication.java
@@ -0,0 +1,93 @@
+package com.daruma_app;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+import com.facebook.react.PackageList;
+import com.reactnativenavigation.NavigationApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+// import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
+
+public class MainApplication extends NavigationApplication {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    private final ReactNativeHost mReactNativeHost =
+            new NavigationReactNativeHost(this) {
+                @Override
+                public boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
+
+                @Override
+                protected List<ReactPackage> getPackages() {
+                    @SuppressWarnings("UnnecessaryLocalVariable")
+                    List<ReactPackage> packages = new PackageList(this).getPackages();
+                    // packages.add(new ReactNativeFirebaseMessagingPackage());
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    // packages.add(new MyReactNativePackage());
+                    return packages;
+                }
+
+                @Override
+                protected String getJSMainModuleName() {
+                    return "index";
+                }
+            };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    }
+
+    /**
+     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+     *
+     * @param context
+     * @param reactInstanceManager
+     */
+    private static void initializeFlipper(
+            Context context, ReactInstanceManager reactInstanceManager) {
+        if (BuildConfig.DEBUG) {
+            try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+                Class<?> aClass = Class.forName("com.daruma_app.ReactNativeFlipper");
+                aClass
+                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                        .invoke(null, context, reactInstanceManager);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
